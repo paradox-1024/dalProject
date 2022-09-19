0635ec0f9fb24f4974a78867cd81f8a9b968c5f0
paradox-1024 <83621225+paradox-1024@users.noreply.github.com>
2022-09-19 18:37:38 -0300

    Update Demo.java

--- java-review/Demo.java
+++ java-review/Demo.java
@@ -16,18 +16,18 @@ public class Demo {
             Player p2 = new Player("Bob");
             f = new Field(0.0, 0.0, 200.0, 300.0);
             b = new Ball(100.0, 100.0);
-//repeat 10 kick each
+            //repeat 10 kick each
             for (int i = 0; i < 10; i++) {
                 double d = random.nextDouble() * f.getWidth();
                 double r = random.nextDouble() * 360;
                 p1.kick(f, b, d, r);
-                //System.out.println("Field: "+f.toString());
-               // System.out.println("Ball is at "+b.toString());
+                System.out.println("Field: "+f.toString());
+               System.out.println("Ball is at "+b.toString());
 
                 d = random.nextDouble() * f.getWidth();
                 r = random.nextDouble() * 360;
                 p2.kick(f, b, d, r);
-               // System.out.println("Ball is at "+b.toString());
+               System.out.println("Ball is at "+b.toString());
 
 
             }
