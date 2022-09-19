3658fb59dd1a50c08cf2d468670d23705d37ee92
paradox-1024 <83621225+paradox-1024@users.noreply.github.com>
2022-09-19 19:59:15 -0300

    Delete Android-Mobile-Application directory

--- Android-Mobile-Application/app/src/main/java/com/app/test_nav/Communicator.kt
+++
@@ -1,7 +0,0 @@
-package com.app.test_nav
-
-import com.app.test_nav.models.PostInfo
-
-interface Communicator {
-    fun passData(postId:String)
-}
--- Android-Mobile-Application/app/src/main/java/com/app/test_nav/models/UserModel.kt
+++
@@ -1,7 +0,0 @@
-package com.app.test_nav.models
-
-data class UserModel(var userName:String = "", var userEmail:String = ""){
-    override fun toString(): String {
-        return super.toString()
-    }
-}
